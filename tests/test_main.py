b28beb297e001d07ff5c8f3e237125a84473ca90
sirishavadlamudi <vsirisha028@gmail.com>
2025-11-06 09:31:33 +0530

    Update test for new welcome message

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,4 +8,4 @@ client = TestClient(app)
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello from FastAPI on GKE with CI/CD!"}
+    assert response.json() == {"message": "Hello from FastAPI on GKE with CI pipeline + Argo CD CD Deployment"}
